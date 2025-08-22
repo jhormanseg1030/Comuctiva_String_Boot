4c17ccaad4409544f02fc231a5876ab14130a661
jhorman <jhormansegura2012@gmail.com>
2025-08-22 16:11:44 -0500

    Camila_Zangana

---
+++ src/main/java/com/comuctiva/comuctiva/ComuctivaApplication.java
@@ -0,0 +1,13 @@
+package com.comuctiva.comuctiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComuctivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComuctivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comuctiva/comuctiva/ComuctivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comuctiva.comuctiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComuctivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
