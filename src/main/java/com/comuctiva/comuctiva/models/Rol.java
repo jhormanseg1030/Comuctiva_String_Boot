d17b4080530e021f8133e7cedfe2eda401bddabb
Mari <mariadelmarsanchez802@gmail.com>
2025-08-26 15:28:10 -0500

    Rol

---
+++ src/main/java/com/comuctiva/comuctiva/models/Rol.java
@@ -0,0 +1,21 @@
+package com.comuctiva.comuctiva.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Rol {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer ID_Rol;
+
+@Column(nullable = false, length = 30)
+private String Nom_Rol;
+}
