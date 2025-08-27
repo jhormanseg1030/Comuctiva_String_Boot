f02b0792429475c2d182765c16c5410eb7a2bb28
Mari <mariadelmarsanchez802@gmail.com>
2025-08-27 15:25:34 -0500

    Rol_usu

---
+++ src/main/java/com/comuctiva/comuctiva/models/Rol_Usuario.java
@@ -0,0 +1,20 @@
+package com.comuctiva.comuctiva.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Rol_Usuario {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Boolean Estado;
+
+}
