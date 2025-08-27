8a5136b8770839c2474f5c8c72a439aad8108f7d
Mari <mariadelmarsanchez802@gmail.com>
2025-08-27 15:51:05 -0500

    getter

--- src/main/java/com/comuctiva/comuctiva/models/Rol_Usuario.java
+++ src/main/java/com/comuctiva/comuctiva/models/Rol_Usuario.java
@@ -5,12 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
 @Entity
 public class Rol_Usuario {
 @Id
