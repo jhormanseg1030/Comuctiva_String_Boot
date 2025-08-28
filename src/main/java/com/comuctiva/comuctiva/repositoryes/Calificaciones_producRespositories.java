fcd66a61749fa5e13f5742fa6aaab3efafcd4c37
mari <mariadelmarsanchez802@gmail.com>
2025-08-28 12:14:00 -0500

    Calificaciones_Tienda

---
+++ src/main/java/com/comuctiva/comuctiva/repositoryes/Calificaciones_producRespositories.java
@@ -0,0 +1,9 @@
+package com.comuctiva.comuctiva.repositoryes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.comuctiva.comuctiva.models.Calilficaciones_produc;
+
+public interface Calificaciones_producRespositories extends JpaRepository <Calilficaciones_produc, Integer> {
+
+}
