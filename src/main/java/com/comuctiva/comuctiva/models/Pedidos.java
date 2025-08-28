967030508426db3476d6c4198cc201f9afbc727f
jhorman <jhormansegura2012@gmail.com>
2025-08-28 16:30:30 -0500

    foranea_Pedido

--- src/main/java/com/comuctiva/comuctiva/models/Pedidos.java
+++ src/main/java/com/comuctiva/comuctiva/models/Pedidos.java
@@ -2,10 +2,15 @@ package com.comuctiva.comuctiva.models;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,4 +24,16 @@ public class Pedidos {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id_pedido;
     private LocalDateTime fehor_pedi;
+
+    @ManyToOne (optional = false)
+    @JoinColumn (name = "id_Usuario", nullable = false, foreignKey = @ForeignKey(name = "FK_Usuario"))
+    private Usuario usuario;
+
+    @OneToOne (cascade = CascadeType.ALL)
+    @JoinColumn (name = "id_guia", nullable = false, foreignKey = @ForeignKey(name ="FK_guia_envio"))
+    private Guia_Envio guia_envio;
+
+    @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn (name = "id_Estado", nullable = false, foreignKey = @ForeignKey(name = "FK_Estado"))
+    private Estado estado;
 }
