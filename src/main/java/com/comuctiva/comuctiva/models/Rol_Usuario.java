51d3775604eb491b52cdab4d207b00021385ba54
jhorman <jhormansegura2012@gmail.com>
2025-08-28 16:37:54 -0500

    ..

--- src/main/java/com/comuctiva/comuctiva/models/Rol_Usuario.java
+++ src/main/java/com/comuctiva/comuctiva/models/Rol_Usuario.java
@@ -1,6 +1,8 @@
 package com.comuctiva.comuctiva.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
