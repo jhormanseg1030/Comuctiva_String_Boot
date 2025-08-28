38881d4c5082c0a8d8bd7e8dfd6935b431c3ed8b
camila <camilacasttro.25@gmail.com>
2025-08-28 17:06:12 -0500

    fcompra

--- src/main/java/com/comuctiva/comuctiva/models/Compra.java
+++ src/main/java/com/comuctiva/comuctiva/models/Compra.java
@@ -2,11 +2,16 @@ package com.comuctiva.comuctiva.models;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,4 +28,12 @@ public class Compra {
     @Column (length = 30)
     private String ref_pago;
     private LocalDateTime fec_com;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn (name = "id_pedido", nullable = false, foreignKey = @ForeignKey(name = "FK_Pedido"))
+    private Pedidos pedido;
+
+    @ManyToOne (cascade = CascadeType.ALL)
+    @JoinColumn (name = "id_TiPago", nullable = false, foreignKey = @ForeignKey(name = "FK_TiPago"))
+    private Tipo_De_Pago tipo_pago;
 }
