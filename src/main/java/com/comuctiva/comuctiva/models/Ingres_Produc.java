04deb7a798368cc57229adf26834da4e7c8b6f65
Camila <camilacasttro.25@gmail.com>
2025-08-28 10:57:40 -0500

    Ingre_Produc

---
+++ src/main/java/com/comuctiva/comuctiva/models/Ingres_Produc.java
@@ -0,0 +1,14 @@
+package com.comuctiva.comuctiva.models;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Ingres_Produc {
+    private Short cant;
+}
