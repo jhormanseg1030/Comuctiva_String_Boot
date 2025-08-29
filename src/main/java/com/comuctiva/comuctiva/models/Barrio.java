99d92ca72287ec46506527814281ec9d6711edfb
jhorman <jhormansegura2012@gmail.com>
2025-08-29 14:39:20 -0500

    listo

--- src/main/java/com/comuctiva/comuctiva/models/Barrio.java
+++ src/main/java/com/comuctiva/comuctiva/models/Barrio.java
@@ -1,10 +1,14 @@
 package com.comuctiva.comuctiva.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,4 +23,12 @@ public class Barrio {
     private Integer id_barrio;
     @Column (length=50)
     private String nom;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_Muni", nullable = false, foreignKey = @ForeignKey(name = "FK_Muni"))
+    private Muni muni;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_Barr_vere", nullable = false, foreignKey = @ForeignKey(name = "Fk_Barr_Vere"))
+    private Barr_Vere barr_Vere;
 }
