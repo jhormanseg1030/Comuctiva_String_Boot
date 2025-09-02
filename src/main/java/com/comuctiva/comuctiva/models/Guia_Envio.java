bd5e3373c93ac6972e1e31f23df09d06ced33567
jhorman <jhormansegura2012@gmail.com>
2025-09-02 14:52:13 -0500

    foreana_Guia

--- src/main/java/com/comuctiva/comuctiva/models/Guia_Envio.java
+++ src/main/java/com/comuctiva/comuctiva/models/Guia_Envio.java
@@ -1,10 +1,14 @@
 package com.comuctiva.comuctiva.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +20,15 @@ import lombok.NoArgsConstructor;
 public class Guia_Envio {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-private Integer id_guia;
-@Column (length=50)
-private String fec_env;
+    private Integer id_guia;
+    @Column (length=50)
+    private String fec_env;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_Transpor", nullable = false, foreignKey = @ForeignKey(name = "FK_Transpor"))
+    private Transportadora transportadora;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "Id_Obser", nullable = false, foreignKey = @ForeignKey(name = "FK_Obser"))
+    private Obser obser;
 }
