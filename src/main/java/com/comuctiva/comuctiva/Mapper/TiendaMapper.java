1f7c5913523783ba053a4f3f497752261c866d20
lore_y_Mari <hernandezpoqu@gmail.com>
2025-09-04 16:48:30 -0500

    MM

--- src/main/java/com/comuctiva/comuctiva/Mapper/TiendaMapper.java
+++ src/main/java/com/comuctiva/comuctiva/Mapper/TiendaMapper.java
@@ -1,5 +1,13 @@
 package com.comuctiva.comuctiva.Mapper;
 
-public interface TiendaMapper {
+import java.util.List;
 
+import com.comuctiva.comuctiva.Dto.TiendaDto;
+import com.comuctiva.comuctiva.models.Tienda;
+
+public interface TiendaMapper {
+Tienda toTienda(TiendaDto tiendaDto);
+TiendaDto toTiendaDto(Tienda tienda);
+List<TiendaDto> toTiendaDtoList(List<Tienda>tiendas);
+void updateTienda(Tienda tienda, TiendaDto tiendaDto);
 }
