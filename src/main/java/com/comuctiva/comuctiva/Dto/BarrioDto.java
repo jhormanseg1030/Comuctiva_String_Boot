82c5c6bb949385534382073bddf54f94c3a16f27
jhorman <jhormansegura2012@gmail.com>
2025-09-04 17:37:29 -0500

    primer_intento_controller

--- src/main/java/com/comuctiva/comuctiva/Dto/BarrioDto.java
+++ src/main/java/com/comuctiva/comuctiva/Dto/BarrioDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class BarrioDto {
-private Integer id_barr;
-private String nomb;
+    private Integer id_barr;
+    private String nomb;
 }
