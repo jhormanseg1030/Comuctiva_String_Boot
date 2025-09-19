26915c8d7b8fed4bb814e9295e2ce807a28dd748
jhorman <jhormansegura2012@gmail.com>
2025-09-19 09:38:29 -0500

    Guia_Envio_Listo

--- src/main/java/com/comuctiva/comuctiva/controller/Guia_envioController.java
+++ src/main/java/com/comuctiva/comuctiva/controller/Guia_envioController.java
@@ -1,11 +1,13 @@
 package com.comuctiva.comuctiva.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.comuctiva.comuctiva.Dto.Guia_EnvioCrearDtos;
 import com.comuctiva.comuctiva.Dto.Guia_EnvioDto;
 import com.comuctiva.comuctiva.services.Guia_EnvioServices;
 
@@ -21,8 +23,8 @@ public class Guia_envioController {
     }
 
     @PostMapping("/crearGuia")
-    public ResponseEntity<Guia_EnvioDto> crearGuia_Envio( @Valid @RequestBody Guia_EnvioDto guia_EnvioDto) {
-        Guia_EnvioDto nuevaGuia_Envio = guia_EnvioServices.crearGuia_Envio(guia_EnvioDto);
-        return ResponseEntity.ok(nuevaGuia_Envio);
+    public ResponseEntity<Guia_EnvioDto> crearGuia_Envio( @Valid @RequestBody Guia_EnvioCrearDtos guia_EnvioCrearDtos) {
+        Guia_EnvioDto nuevaGuia_Envio = guia_EnvioServices.crearGuia_Envio(guia_EnvioCrearDtos);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaGuia_Envio);
     }
 }
