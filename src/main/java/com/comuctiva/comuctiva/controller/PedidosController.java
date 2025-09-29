feb6ec9a2f40a388b734c994604e689e7e04687d
camila <camilacasttro.25@gmail.com>
2025-09-29 15:50:04 -0500

    pedidos_vias

--- src/main/java/com/comuctiva/comuctiva/controller/PedidosController.java
+++ src/main/java/com/comuctiva/comuctiva/controller/PedidosController.java
@@ -1,5 +1,6 @@
 package com.comuctiva.comuctiva.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -10,10 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.comuctiva.comuctiva.Dto.PedidoCreateDto;
+import com.comuctiva.comuctiva.Dto.PedidoUpdateDto;
 import com.comuctiva.comuctiva.Dto.PedidosDto;
 import com.comuctiva.comuctiva.services.PedidosServices;
 
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -24,7 +31,9 @@ public class PedidosController {
     public PedidosController(PedidosServices pedidosServices) {
         this.pedidosServices = pedidosServices;
     }
-    @PostMapping("/crearemos")
+
+    //Crear un pedido
+    @PostMapping
     public ResponseEntity<?> crearPedido( @Valid @RequestBody PedidoCreateDto pedidoCreateDto){
         try{
                 PedidosDto pedidosDto = pedidosServices.crearPedidos(pedidoCreateDto);
@@ -32,11 +41,34 @@ public class PedidosController {
                 .body(Map.of("mensaje", "Pedido creado con exito", "pedidos", pedidosDto));
         } catch(IllegalStateException ex){
             return ResponseEntity.status(HttpStatus.CONFLICT)
-            .body(Map.of("mensaje", ex.getMessage()));
+            .body(Map.of("errores1", ex.getMessage()));
         } catch(Exception ex){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(Map.of("error","error al crear un pedido", "detalles", ex.getMessage()));
         }
+    }
+
+    //consultar pedidos por id
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidosDto> consultarPorId(@PathVariable Integer id_pedido){
+        PedidosDto pedidos = pedidosServices.pedidosPorId(id_pedido);
+        return ResponseEntity.ok(pedidos);
+    }
+
+    //listar pedidos
+    @GetMapping
+    public ResponseEntity<List<PedidosDto>> listartodos(){
+        List<PedidosDto> pedidos =pedidosServices.listartodos();
+        return ResponseEntity.ok(pedidos);
+    }
+
+    //actualizar pedido
+    @PutMapping("/{id}")
+    public ResponseEntity<PedidosDto> actualizarPedido(@PathVariable Integer id_pedido,
+    @Valid @RequestBody PedidoUpdateDto pediActualizadoDto){
+        pediActualizadoDto.setEstId(id_pedido);
+        PedidosDto pedidosActualizado = pedidosServices.actualizarPedidos(pediActualizadoDto);
+        return ResponseEntity.ok(pedidosActualizado);
     }
 }
 
